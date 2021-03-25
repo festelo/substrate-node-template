3c3e6e4fe12704992517d000364dabcf9a218980
Ilya Beregovskiy <festeloqq@gmail.com>
2021-03-25 14:33:59 +0300

    bug reproducing

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,11 +1,27 @@
 use sp_core::{Pair, Public, sr25519};
 use node_template_runtime::{
-	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig,
-	SudoConfig, SystemConfig, WASM_BINARY, Signature
+	AccountId, BalancesConfig, 
+	wasm_binary_unwrap,
+    TechnicalMembershipConfig,
+    BabeConfig,
+    GenesisConfig,
+    GrandpaConfig,
+    IndicesConfig,
+    SessionConfig,
+    SessionKeys,
+    Signature,
+    StakerStatus,
+    StakingConfig,
+    SudoConfig,
+    SystemConfig,
 };
-use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_finality_grandpa::AuthorityId as GrandpaId;
+use sp_consensus_babe::AuthorityId as BabeId;
 use sp_runtime::traits::{Verify, IdentifyAccount};
+pub use sp_runtime::{
+    Perbill,
+    Permill,
+};
 use sc_service::ChainType;
 
 // The URL for the telemetry server.
@@ -31,15 +47,16 @@ pub fn get_account_id_from_seed<TPublic: Public>(seed: &str) -> AccountId where
 }
 
 /// Generate an Aura authority key.
-pub fn authority_keys_from_seed(s: &str) -> (AuraId, GrandpaId) {
+pub fn authority_keys_from_seed(seed: &str) -> (AccountId, AccountId, GrandpaId, BabeId) {
 	(
-		get_from_seed::<AuraId>(s),
-		get_from_seed::<GrandpaId>(s),
+        get_account_id_from_seed::<sr25519::Public>(&format!("{}//stash", seed)),
+        get_account_id_from_seed::<sr25519::Public>(seed),
+        get_from_seed::<GrandpaId>(seed),
+        get_from_seed::<BabeId>(seed),
 	)
 }
 
 pub fn development_config() -> Result<ChainSpec, String> {
-	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm binary not available".to_string())?;
 
 	Ok(ChainSpec::from_genesis(
 		// Name
@@ -48,7 +65,6 @@ pub fn development_config() -> Result<ChainSpec, String> {
 		"dev",
 		ChainType::Development,
 		move || testnet_genesis(
-			wasm_binary,
 			// Initial PoA authorities
 			vec![
 				authority_keys_from_seed("Alice"),
@@ -77,58 +93,23 @@ pub fn development_config() -> Result<ChainSpec, String> {
 	))
 }
 
-pub fn local_testnet_config() -> Result<ChainSpec, String> {
-	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm binary not available".to_string())?;
-
-	Ok(ChainSpec::from_genesis(
-		// Name
-		"Local Testnet",
-		// ID
-		"local_testnet",
-		ChainType::Local,
-		move || testnet_genesis(
-			wasm_binary,
-			// Initial PoA authorities
-			vec![
-				authority_keys_from_seed("Alice"),
-				authority_keys_from_seed("Bob"),
-			],
-			// Sudo account
-			get_account_id_from_seed::<sr25519::Public>("Alice"),
-			// Pre-funded accounts
-			vec![
-				get_account_id_from_seed::<sr25519::Public>("Alice"),
-				get_account_id_from_seed::<sr25519::Public>("Bob"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie"),
-				get_account_id_from_seed::<sr25519::Public>("Dave"),
-				get_account_id_from_seed::<sr25519::Public>("Eve"),
-				get_account_id_from_seed::<sr25519::Public>("Ferdie"),
-				get_account_id_from_seed::<sr25519::Public>("Alice//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Bob//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Charlie//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Dave//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
-				get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
-			],
-			true,
-		),
-		// Bootnodes
-		vec![],
-		// Telemetry
-		None,
-		// Protocol ID
-		None,
-		// Properties
-		None,
-		// Extensions
-		None,
-	))
+fn session_keys(
+    grandpa: GrandpaId,
+    babe: BabeId,
+    // im_online: ImOnlineId,
+) -> SessionKeys {
+    SessionKeys {
+        grandpa,
+        babe,
+        // im_online,
+    }
 }
 
+const INITIAL_BALANCE: u128 = 8_750_000_000_000_000_000_000_u128; // $70M 70_000_000_000_000_000_000_000_u128
+const INITIAL_STAKING: u128 = 1_000_000_000_000_000_000_u128;
 /// Configure initial storage state for FRAME modules.
 fn testnet_genesis(
-	wasm_binary: &[u8],
-	initial_authorities: Vec<(AuraId, GrandpaId)>,
+	initial_authorities: Vec<(AccountId, AccountId, GrandpaId, BabeId)>,
 	root_key: AccountId,
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
@@ -136,22 +117,63 @@ fn testnet_genesis(
 	GenesisConfig {
 		frame_system: Some(SystemConfig {
 			// Add Wasm runtime to storage.
-			code: wasm_binary.to_vec(),
+            code: wasm_binary_unwrap().to_vec(),
 			changes_trie_config: Default::default(),
 		}),
 		pallet_balances: Some(BalancesConfig {
 			// Configure endowed accounts with initial balance of 1 << 60.
 			balances: endowed_accounts.iter().cloned().map(|k|(k, 1 << 60)).collect(),
 		}),
-		pallet_aura: Some(AuraConfig {
-			authorities: initial_authorities.iter().map(|x| (x.0.clone())).collect(),
-		}),
-		pallet_grandpa: Some(GrandpaConfig {
-			authorities: initial_authorities.iter().map(|x| (x.1.clone(), 1)).collect(),
-		}),
+        pallet_indices: Some(IndicesConfig {
+            indices: endowed_accounts.iter().enumerate().map(|(index, x)| (index as u32, (*x).clone())).collect(),
+        }),
+        pallet_session: Some(SessionConfig {
+            keys: initial_authorities
+                .iter()
+                .map(|x| (x.0.clone(), x.0.clone(), session_keys(x.2.clone(), x.3.clone())))
+                .collect::<Vec<_>>(),
+        }),
+        pallet_staking: Some(StakingConfig {
+            validator_count: initial_authorities.len() as u32 * 2,
+            minimum_validator_count: initial_authorities.len() as u32,
+            stakers: initial_authorities
+                .iter()
+                .map(|x| (x.0.clone(), x.1.clone(), INITIAL_STAKING, StakerStatus::Validator))
+                .collect(),
+            invulnerables: initial_authorities.iter().map(|x| x.0.clone()).collect(),
+            slash_reward_fraction: Perbill::from_percent(10),
+            ..Default::default()
+        }),
 		pallet_sudo: Some(SudoConfig {
 			// Assign network admin rights.
-			key: root_key,
+			key: root_key.clone(),
 		}),
+        pallet_babe: Some(BabeConfig {
+            authorities: vec![],
+        }),
+        pallet_grandpa: Some(GrandpaConfig {
+            authorities: vec![],
+        }),
+        pallet_collective_Instance1: Some(Default::default()),
+        pallet_membership_Instance1: Some(TechnicalMembershipConfig {
+            members: vec![root_key.clone()],
+            phantom: Default::default(),
+        }),
+        pallet_collective_Instance2: Some(Default::default()),
+        // pallet_tips: Some(Default::default()),
+        pallet_treasury: Some(Default::default()),
 	}
+}
+
+pub fn load_spec(id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
+	let option = if id == "dev" {
+		Some(development_config().unwrap())
+	} else { None };
+
+    let spec = Box::new(match option {
+        Some(v) => v,
+        None => ChainSpec::from_json_file(std::path::PathBuf::from(id))?,
+    }) as Box<dyn sc_service::ChainSpec>;
+
+    return Ok(spec);
 }
